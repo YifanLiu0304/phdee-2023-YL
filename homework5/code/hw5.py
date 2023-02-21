5740877e8676629f311fb1d994f20d611c4b36c6
Yifan LIU <yliu3494@gatech.edu>
2023-02-21 17:36:06 -0500

    HW5

--- homework5/code/hw5.py
+++ homework5/code/hw5.py
@@ -49,16 +49,66 @@ with open("Q1.tex", "w") as f: f.write(ols.summary().as_latex())
 
 
 # Q2: 
-# The price also affects the fuel effciency in miles per gallon.
+# The endogeneity might result from simultaneity.
+# While the fuel efficiency affects the price, the price also affects the fuel effciency in miles per gallon.
+# In other words, the fuel efficiency and the price are determined by each other.
+# A change in error term causes the price to change, which causes the fuel efficiency to change. Therefore, the error term and the fuel efficiency are not independent.
+
 
 # Q3: IV
-# (a)
+# add a column of ones named "one"
+df = df.assign(one = pd.Series(1, index=df.index))
+# (a) "weight" as the excluded instrument
+# regress X on Z
+# PWX = Z*inv(Z'*Z)*Z'*X
+Z1 = df[['one','weight','car']]
+X = df[['one','mpg','car']]
+PWX1 = np.matrix (Z1 @ np.linalg.inv(Z1.T @ Z1)) @ np.matrix(Z1.T) @ X
+# regress y on the fitted value 
+# beta_2sls = inv(PWX'*PWX)*PWX'*y
+y = df['price']
+beta1 = np.linalg.inv(PWX1.T @ PWX1) @ PWX1.T @ y
+
+
 # (b)
+df['weight2'] = df['weight'].apply(lambda x: x**2) 
+# regress X on Z
+# PWX = Z*inv(Z'*Z)*Z'*X
+Z2 = df[['one','weight2','car']]
+X = df[['one','mpg','car']]
+PWX2 = np.matrix (Z2 @ np.linalg.inv(Z2.T @ Z2)) @ np.matrix(Z2.T) @ X
+# regress y on the fitted value 
+y = df['price']
+beta2 = np.linalg.inv(PWX2.T @ PWX2) @ PWX2.T @ y
+
+
 # (c)
+# regress X on Z
+# PWX = Z*inv(Z'*Z)*Z'*X
+Z3 = df[['one','height','car']]
+X = df[['one','mpg','car']]
+PWX3 = np.matrix (Z3 @ np.linalg.inv(Z3.T @ Z3)) @ np.matrix(Z3.T) @ X
+# regress y on the fitted value 
+y = df['price']
+beta3 = np.linalg.inv(PWX3.T @ PWX3) @ PWX3.T @ y
+
+# (d)
+# (e)
+beta = pd.concat([beta1, beta2, beta3], axis=1)
+# change the row names
+new_index = ['constant', 'mpg', 'car']
+beta.index = new_index
+# change the column names
+new_columns = ['weight as IV', 'weight2 as IV','height as IV']
+beta.columns = new_columns
+beta
+
+## Output directly to LaTeX
+os.chdir(outputpath) # Output directly to LaTeX folder
+beta.to_latex('3e.tex')
 
 
-
-
+# Q4: IVGMM
 
 
 
